1f8237f93216c2f6a81c7eae675ca297db76ae8c
Sarvesh Singtamkar <sarvesh8327@gmail.com>
2026-03-11 13:56:47 +0530

    Coded UC1: Display Hello World in console

---
+++ HelloApp.java
@@ -0,0 +1,17 @@
+/**
+ * HelloApp.java – A simple Java application that displays "Hello World"
+ * to the console.
+ *
+ * @author Sarvesh
+ * @version 1.0
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
